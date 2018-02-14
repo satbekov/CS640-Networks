d2572ebd539f8da88d38852b9473070788a9a686
Kairat Satbekov <satbekov@wisc.edu>
2018-02-14 16:27:52 -0600

    server side

--- P1/iperfer/Server.java
+++ P1/iperfer/Server.java
@@ -1,4 +1,44 @@
 package P1.iperfer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    private void printSummary(long time, long data) {
+        time = System.currentTimeMillis() - time;
+        time = time / 1000;
+        data = data / 1000;
+        double trafficRate = (data * 8) / (time * 1000);
+        System.out.println("received=" + data + "KB rate=" + String.format("%.3f", trafficRate) + " Mbps");
+    }
+
+    public void establishConnection() throws IOException {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            byte streamData[] = new byte[1000];
+            long data = 0;
+            long bytesRead;
+            long time = System.currentTimeMillis();
+
+            InputStream inStream = clientSocket.getInputStream();
+            while ((bytesRead = inStream.read(streamData)) != -1) {
+                data = data + bytesRead;
+            }
+            printSummary(time, data);
+            serverSocket.close();
+            clientSocket.close();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new IOException();
+        }
+    }
 }
--- P1/iperfer/TestIperfer.java
+++ P1/iperfer/TestIperfer.java
@@ -1,4 +1,101 @@
 package P1.iperfer;
 
+import java.io.IOException;
+
 public class TestIperfer {
+
+    private static void printError() {
+        System.err.println("Error: missing or additional arguments");
+        System.exit(1);
+    }
+
+    private static void printPortError() {
+        System.err.println("Error: port number must be in the range 1024 to 65535");
+        System.exit(-1);
+    }
+
+    private static void checkArguments(String[] cargs, boolean isServer, boolean isClient) {
+
+        for(int count = 0; count < cargs.length; count++) {
+            if(cargs[count].equals("-s")) {
+                if(cargs.length != 3) {
+                    printError();
+                }
+                isServer = true;
+                break;
+            }
+            if(cargs[count].equals("-c")) {
+                if(cargs.length != 7) {
+                    printError();
+                }
+                isClient = true;
+                break;
+            }
+        }
+
+        if((isClient || isServer) == false) {
+            printError();
+        }
+    }
+
+    private static void handleArguments(String[] cargs, boolean isServer, boolean isClient) throws IOException {
+        long time = 0;
+        int port = 0;
+        String host = "";
+
+        for(int count = 0; count < cargs.length && cargs[count].startsWith("-"); count++) {
+            String arg = cargs[count];
+            if (arg.charAt(1) == 'c' || arg.charAt(1) == 's')
+                continue;
+            else {
+                switch (arg) {
+                    case "p":
+                        port = Integer.parseInt(cargs[count++]);
+                        if (port <= 1024 || port >= 65535) {
+                            printPortError();
+                        }
+                        break;
+                    case "h":
+                        if(!isClient) {
+                            printError();
+                        } else {
+                            host = cargs[count++];
+                        }
+                        break;
+                    case "t":
+                        if(!isClient) {
+                            printError();
+                        } else {
+                            time = Integer.parseInt(cargs[count++]);
+                        }
+                        break;
+                    default:
+                        printError();
+                }
+            }
+        }
+
+        if(isServer) {
+            Server server = new Server(port);
+            try {
+                server.establishConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(isClient) {
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        boolean isServer = false;
+        boolean isClient = false;
+
+        checkArguments(args, isServer, isClient);
+        handleArguments(args, isServer, isClient);
+
+    }
+
 }
